c2502e8017134baf9c42d84ca6ac0716db311aeb
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-10-23 13:25:02 +0200

    Create firstTryp5.js

---
+++ firstTryp5.js
@@ -0,0 +1,14 @@
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+function draw(){
+  background(200,100,50);
+  
+  fill(40,20,255);
+  ellipse(40,40,90,90);
+  
+  fill(10,100,90);
+  rect(300,200,90,90);
+}
