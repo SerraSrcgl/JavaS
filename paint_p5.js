b9612599b5d81fa2b43327f65d80a2b341541164
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-10-24 17:58:13 +0200

    Create paint_p5.js

---
+++ paint_p5.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background(155,203,30);
+}
+
+function draw() {
+  
+  
+  
+  fill(100,100,50)
+  ellipse(mouseX,mouseY,20,20);
+}
+function mousePressed(){
+  background(155,203,30);
+}
