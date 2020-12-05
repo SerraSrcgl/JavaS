1104c1a7c72f7472af6055c34dd92b7fc462f49b
Serra Saracoglu <62646936+SerraSrcgl@users.noreply.github.com>
2020-12-05 23:07:21 +0100

    Create withTimer.js

---
+++ codeForFutureHelp/withTimer.js
@@ -0,0 +1,3 @@
+const paragraph = document.createElement('p')
+        paragraph.textContent = 'hello my name is serra i dont like people who dont keep their promises'
+        setTimeout(() => { document.body.appendChild(paragraph) }, 5000)
